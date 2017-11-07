a0aea01c564b57e3b033a297efff9302762255ba
Fabio Cumbo <fabio.cumbo@gmail.com>
2017-11-07 14:48:24 -0500

    GalaxyDataRetrieval update 20171107

--- background.js
+++ background.js
@@ -1,45 +1,12 @@
-
 // Send selected data to Galaxy.
 function sendToGalaxy(galaxy_url, galaxy_user, galaxy_pass, checkedLinks) {
-    /*var galaxy_url = document.getElementById('galaxy').value;
-    console.log("galaxy_url: "+galaxy_url);
-    var galaxy_user = document.getElementById('user').value;
-    console.log("galaxy_user: "+galaxy_user);
-    var galaxy_pass = document.getElementById('password').value;
-    console.log("galaxy_pass: "+galaxy_pass);*/
-
-    //if (countSelectedLinks(visibleLinks) > 0) {
-        // retrieve api key
-        var api_key = retrieveApiKey(galaxy_url, galaxy_user, galaxy_pass, checkedLinks);
-        /*console.log("api_key: "+api_key);
-        if (api_key !== undefined) {
-            // get current history id
-            var history_id = getCurrentHistoryId(galaxy_url);
-            console.log("history_id: "+history_id);
-            if (history_id !== undefined) {
-                for (var i = 0; i < visibleLinks.length; ++i) {
-                    if (document.getElementById('check' + i).checked) {
-                        // send data to galaxy
-                        var file_url = visibleLinks[i];
-                        console.log("file_url: "+file_url);
-                        var file_name = file_url.split("/").pop();
-                        var file_type = "auto";
-                        var dbkey = "?";
-                        uploadData(galaxy_url, api_key, history_id, file_name, file_url, file_type, dbkey);
-                    }
-                }
-            }
-            else console.log("Unable to retrieve the current history id.");
-        }
-        else console.log("Unable to retrieve api key.");*/
-    //}
-    //else console.log("Select at least one link.");
-
-    //window.close();
+    // start by retrieving the api key for the selected user
+    // then retrieve the current history id
+    // finally send links to the upload tool
+    retrieveApiKey(galaxy_url, galaxy_user, galaxy_pass, checkedLinks);
 }
 
 function retrieveApiKey(galaxy_url, galaxy_user, galaxy_pass, checkedLinks) {
-    //function retrieveApiKey(galaxy_url, galaxy_user, galaxy_pass) {
     var path = "api/authenticate/baseauth";
     if (!galaxy_url.endsWith("/"))
         path = "/"+path;
@@ -64,7 +31,6 @@ function retrieveApiKey(galaxy_url, galaxy_user, galaxy_pass, checkedLinks) {
 }
 
 function getCurrentHistoryId(galaxy_url, api_key, checkedLinks) {
-    //function getCurrentHistoryId(galaxy_url) {
     var path = "api/histories/most_recently_used";
     if (!galaxy_url.endsWith("/"))
         path = "/"+path;
